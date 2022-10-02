b5933531f5370eac98953ab56dfd0ad7fa69b856
miwayuri <keaitaromi@gmail.com>
2022-10-02 14:27:10 +0900

    second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,18 +7,46 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create
+  def create 
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+      flash[:notice] = "スケジュールを登録しました"
+    else
+      render "new"
+      flash[:alert] = "スケジュールを登録できませんでした"
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+      flash[:notice] = "スケジュールを更新しました"
+    else
+      render "edit"
+      flash[:alert] = "スケジュールを更新できませんでした"
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "スケジュールを削除しました"
+    redirect_to posts_path
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :start, :finish, :all_day, :memo)
+  end
+
 end
